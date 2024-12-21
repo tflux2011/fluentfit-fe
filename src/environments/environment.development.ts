00e90d406420e870edef5c09547faec40571c97e
tflux2011 <tflux2011@gmail.com>
2024-12-20 23:18:38 -0600

    updated environment api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api/',
+    apiUrl: 'http://3.226.217.163/api/',//'http://localhost:3000/api/',
     appTitle: 'FluentFit (Staging)',
 };
